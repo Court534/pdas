dfc92a42f988687fde3eef1c3d477a8f95520554
Courtney Stow <courney-stow@hotmail.co.uk>
2022-12-14 13:31:26 +0000

    Playing around with images

--- React/pdas/src/App.js
+++ React/pdas/src/App.js
@@ -1,11 +1,20 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
     return (
         <div>
             <div>Personal Digital Assistants</div>
 
-            <ProfileCard title="Alexa" handle="@Alexa99" />
+            <img src={AlexaImage} />
+            <img src={SiriImage} />
+
+            <ProfileCard title="Alexa" handle="@Alexa99" img src={AlexaImage} />
             <ProfileCard title="Cortana" handle="@Cortana32" />
             <ProfileCard title="Siri" handle="@Siri01" />
         </div>
