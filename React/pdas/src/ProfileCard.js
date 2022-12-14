33f005ff51075c49874edc0d28e558265aa211d5
Courtney Stow <courney-stow@hotmail.co.uk>
2022-12-14 12:05:23 +0000

    props and destructuring

--- React/pdas/src/ProfileCard.js
+++ React/pdas/src/ProfileCard.js
@@ -1,8 +1,18 @@
-function ProfileCard(props) {
+function ProfileCard({ title, handle }) {
+    // This is the multiple line version of the destructuring below
+    // const title = props.title;
+    // const handle = props.handle;
+
+    // Destructuring
+    // const { title, handle } = props;
+
+    // We then added it as a object argument in the function
+    // this does the same has the destructuring above 
+
     return (
         <div>
-            <div>Title is {props.title}</div>
-            <div>Handle is {props.handle}</div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
         </div>
     )
 }
