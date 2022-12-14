8a17220b3a9376b5f3bb6f72db7260d9cf9a4d57
Courtney Stow <courney-stow@hotmail.co.uk>
2022-12-14 14:55:46 +0000

    Sorted the styling using bulma

--- React/pdas/src/App.js
+++ React/pdas/src/App.js
@@ -5,15 +5,27 @@ import CortanaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
 
 function App() {
-    return (
-        <div>
-            <div>Personal Digital Assistants</div>
+	return (
+  	<div>
+      <div>Personal Digital Assistants</div>
 
-            <ProfileCard title="Alexa" handle="@Alexa99" image={AlexaImage} />
-            <ProfileCard title="Cortana" handle="@Cortana32" image={CortanaImage} />
-            <ProfileCard title="Siri" handle="@Siri01" image={SiriImage} />
-        </div>
-    )
+				<div className="container">
+					<section className="section">
+						<div className="columns">
+							<div className="column is-4">
+      					<ProfileCard title="Alexa" handle="@Alexa99" image={AlexaImage} />
+							</div>
+							<div className="column is-4">
+        				<ProfileCard title="Cortana" handle="@Cortana32" image={CortanaImage} />
+							</div>
+							<div className="column is-4">
+        				<ProfileCard title="Siri" handle="@Siri01" image={SiriImage} />
+							</div>
+						</div>
+					</section>
+				</div>
+      </div>
+  )
 }
 
 export default App
