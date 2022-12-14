c341af729692e11a97c1488435afcca0de00405a
Courtney Stow <courney-stow@hotmail.co.uk>
2022-12-14 15:12:41 +0000

    Finished the styling on the app

--- React/pdas/src/App.js
+++ React/pdas/src/App.js
@@ -21,7 +21,7 @@ function App() {
 									title="Alexa" 
 									handle="@Alexa99" 
 									image={AlexaImage}
-									desciption="Alexa is a virtual assistant developed by Amazon. It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system."
+									description="Alexa is a virtual assistant developed by Amazon. It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system."
 								/>
 							</div>
 							<div className="column is-4">
@@ -29,7 +29,7 @@ function App() {
 									title="Cortana" 
 									handle="@Cortana32" 
 									image={CortanaImage} 
-									desciption="Cortana is a virtual assistant created by Microsoft for Windows 10, Windows 10 Mobile, Windows Phone 8.1, Invoke smart speaker, Microsoft Band, Xbox One, iOS, Android, Windows Mixed Reality, and Amazon Alexa. Cortana is capable of voice and text input, and can answer questions, make recommendations, and perform actions such as opening apps and setting reminders."
+									description="Cortana is a virtual assistant created by Microsoft for Windows 10, Windows 10 Mobile, Windows Phone 8.1, Invoke smart speaker, Microsoft Band, Xbox One, iOS, Android, Windows Mixed Reality, and Amazon Alexa. Cortana is capable of voice and text input, and can answer questions, make recommendations, and perform actions such as opening apps and setting reminders."
 								/>
 							</div>
 							<div className="column is-4">
@@ -37,7 +37,7 @@ function App() {
 									title="Siri" 
 									handle="@Siri01" 
 									image={SiriImage} 
-									desciption="Siri is a virtual assistant developed by Apple Inc. that is available on Apple devices. Siri can understand natural language voice commands and complete tasks such as making phone calls, sending text messages, opening apps, and setting reminders. Siri can also answer questions, search the web, and provide information such as sports scores, news, and weather."
+									description="Siri is a virtual assistant developed by Apple Inc. that is available on Apple devices. Siri can understand natural language voice commands and complete tasks such as making phone calls, sending text messages, opening apps, and setting reminders. Siri can also answer questions, search the web, and provide information such as sports scores, news, and weather."
 								/>
 							</div>
 						</div>
