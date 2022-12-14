fdecdf6c17d262173c676dcb1f353cb1b866eb83
Courtney Stow <courney-stow@hotmail.co.uk>
2022-12-14 15:06:03 +0000

    Finishing up the styling

--- React/pdas/src/App.js
+++ React/pdas/src/App.js
@@ -7,19 +7,38 @@ import SiriImage from "./images/siri.png";
 function App() {
 	return (
   	<div>
-      <div>Personal Digital Assistants</div>
+			<section className="hero is-info">
+				<div className="hero-body">
+					<p className="title">Personal Digital Assistants</p>
+				</div>
+			</section>
 
 				<div className="container">
 					<section className="section">
 						<div className="columns">
 							<div className="column is-4">
-      					<ProfileCard title="Alexa" handle="@Alexa99" image={AlexaImage} />
+      					<ProfileCard 
+									title="Alexa" 
+									handle="@Alexa99" 
+									image={AlexaImage}
+									desciption="Alexa is a virtual assistant developed by Amazon. It is capable of voice interaction, music playback, making to-do lists, setting alarms, streaming podcasts, playing audiobooks, and providing weather, traffic, sports, and other real-time information, such as news. Alexa can also control several smart devices using itself as a home automation system."
+								/>
 							</div>
 							<div className="column is-4">
-        				<ProfileCard title="Cortana" handle="@Cortana32" image={CortanaImage} />
+        				<ProfileCard 
+									title="Cortana" 
+									handle="@Cortana32" 
+									image={CortanaImage} 
+									desciption="Cortana is a virtual assistant created by Microsoft for Windows 10, Windows 10 Mobile, Windows Phone 8.1, Invoke smart speaker, Microsoft Band, Xbox One, iOS, Android, Windows Mixed Reality, and Amazon Alexa. Cortana is capable of voice and text input, and can answer questions, make recommendations, and perform actions such as opening apps and setting reminders."
+								/>
 							</div>
 							<div className="column is-4">
-        				<ProfileCard title="Siri" handle="@Siri01" image={SiriImage} />
+        				<ProfileCard 
+									title="Siri" 
+									handle="@Siri01" 
+									image={SiriImage} 
+									desciption="Siri is a virtual assistant developed by Apple Inc. that is available on Apple devices. Siri can understand natural language voice commands and complete tasks such as making phone calls, sending text messages, opening apps, and setting reminders. Siri can also answer questions, search the web, and provide information such as sports scores, news, and weather."
+								/>
 							</div>
 						</div>
 					</section>
